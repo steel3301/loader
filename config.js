8f7e1faaedcde6e30261b0d6427fb4eee404d8d3
Kaustub-Warme <kaustubhwarme@gmail.com>
2026-07-20 13:38:56 +0530

    Change3

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.LLM_Config = {
-    apiKey = "sk-or-v1-9bd748f7fe914e16295dde9e983f7022bf517d477132d361331a6e4894c", 
+    apiKey: "sk-or-v1-9bd748f7fe914e16295dde9e983f7022bf517d477132d361331a6e4894c", 
      models: [
         "tencent/hy3:free",
         "nvidia/nemotron-3-ultra-550b-a55b:free",
